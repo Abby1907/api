4a0fddbbe798c836b4cb1f949c2d307715a34e9a
arbajit0901 <arbajitsen0901@gmail.com>
2024-06-23 08:18:55 +0530

    first commit

---
+++ temperatureConvertion.js
@@ -0,0 +1,8 @@
+module.exports = (req, res) => {
+    const input = Number(req.body.input);
+    let result;
+        if (inputTemp) {
+            result = (9/5) * inputTemp + 32;
+            return res.send({success: true, result: result});
+        }
+}
